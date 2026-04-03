71bdde8ddc66b31eaa4cdd42493f8b40d15bb2a3
Nerazuriancta <nerasyahputri@gmail.com>
2026-04-03 20:16:50 +0700

    praktikum2_insertionsort

--- MahasiswaDemo20.java
+++ MahasiswaDemo20.java
@@ -37,5 +37,10 @@ public class MahasiswaDemo20 {
         System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
         list.selectionSort();
         list.tampil();
+
+        System.out.println();
+        System.out.println("Data yang sudah terurut menggunakan INSERTION SORT (ASC)");
+        list.insertionSort();
+        list.tampil();
     }
 }
